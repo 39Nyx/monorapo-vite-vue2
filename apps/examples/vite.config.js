76d48f0d0e43031ff8dd9a452e1acc4d9986df7a
hzj <hezhijian249@qq.com>
2025-07-18 23:10:04 +0800

    feat: 开发环境使用源码，生产使用打包后的文件

--- apps/examples/vite.config.js
+++ apps/examples/vite.config.js
@@ -1,23 +1,34 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import legacy from '@vitejs/plugin-legacy'
 import vue2 from '@vitejs/plugin-vue2'
 import vue2Jsx from '@vitejs/plugin-vue2-jsx'
 
+function getAlias(mode) {
+  if (mode === 'development') {
+    return {
+      '@39nyx/ui-components': fileURLToPath(new URL('../../packages/ui-components/src', import.meta.url))
+    }
+  }
+  return {}
+}
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue2(),
-    vue2Jsx(),
-    legacy({
-      targets: ['ie >= 11'],
-      additionalLegacyPolyfills: ['regenerator-runtime/runtime']
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      vue2(),
+      vue2Jsx(),
+      legacy({
+        targets: ['ie >= 11'],
+        additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        ...getAlias(mode)
+      }
     }
   }
 })
