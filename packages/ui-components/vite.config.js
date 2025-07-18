96765b7a53f8b7e1a2e64cf2ff684ef260c1fb3a
hzj <hezhijian249@qq.com>
2025-07-19 00:22:46 +0800

    feat: 修复部署后样式丢失问题

--- packages/ui-components/vite.config.js
+++ packages/ui-components/vite.config.js
@@ -5,7 +5,25 @@ import vue2Jsx from '@vitejs/plugin-vue2-jsx'
 export default defineConfig({
   plugins: [
     vue2(),
-    vue2Jsx()
+    vue2Jsx(),
+    {
+      name: 'force-css-inject',
+      generateBundle(opts, bundle) {
+        for (const [, chunk] of Object.entries(bundle)) {
+          if (chunk.facadeModuleId && chunk.facadeModuleId.endsWith('.vue')) {
+            const regexp = /scoped_[a-z0-9]+_lang\.js$/
+            const findResult = (chunk.imports || []).find(item => {
+              return regexp.test(item)
+            })
+            if (findResult) {
+              const start = findResult.lastIndexOf('/')
+              const url = findResult.slice(start, findResult.length - 3)
+              chunk.code = `import '.${ url }.css';\n${ chunk.code }`
+            }
+          }
+        }
+      }
+    }
   ],
   build: {
     lib: {
@@ -15,7 +33,7 @@ export default defineConfig({
       formats: ['es']
     },
     cssTarget: ['chrome112'],
-    // css代码切割
+    // 启用CSS代码切割，实现按需引入
     cssCodeSplit: true,
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
@@ -34,7 +52,16 @@ export default defineConfig({
         // 入口文件规则
         entryFileNames: '[name].js',
         // 静态资源规则
-        assetFileNames: 'assets/[name][extname]'
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name.endsWith('&lang.css') && assetInfo.name.startsWith('src/')) {
+            const lastIndex = assetInfo.name.lastIndexOf('/')
+            if (lastIndex !== -1) {
+              const prefix = assetInfo.name.slice(4, lastIndex)
+              return `${ prefix }/[name].css`
+            }
+          }
+          return 'chunks/[name]-[hash].css'
+        }
       }
     }
   }
