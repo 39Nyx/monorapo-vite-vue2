e57eb00de8f5bd28bb5995f21378865ed0f2e701
hzj <hezhijian249@qq.com>
2025-07-18 22:48:32 +0800

    feat: examples安装shared-utils以及简单使用测试

--- apps/examples/src/views/HomeView.vue
+++ apps/examples/src/views/HomeView.vue
@@ -1,9 +1,25 @@
-<script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+<script setup>
+import { computed, ref } from 'vue'
+import { sum } from 'shared-utils'
+
+const count1 = ref(0)
+const count2 = ref(0)
+
+const result = computed(() => {
+  return sum(count1.value, count2.value)
+})
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <div>
+      <button @click="count1++">Increment count1 = {{ count1 }}</button>
+    </div>
+    <div>
+      <button @click="count2++">Increment count2 = {{ count2 }}</button>
+    </div>
+    <div>
+      结果: {{ result }}
+    </div>
+  </div>
 </template>
